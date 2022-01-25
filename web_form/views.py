1a67f484d94222851ba6f9f76799cc5ce1d2e482
kiptoo-rotich <rotichenoch27@gmail.com>
2022-01-25 08:25:02 -0500

    It connects model to views

--- web_form/views.py
+++ web_form/views.py
@@ -1,8 +1,18 @@
 from ast import For
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .forms import Form
+from .models import User
 
 # Create your views here.
 def home(request):
-    form=Form()
-    return render(request,'index.html',{'form':form})
+    data=User.objects.all()
+    if request.method=="POST":
+        form=Form(request.POST,request.FILES)
+        if form.is_valid():
+            data=form.save(commit=False)
+            data.user=request.user
+            data.save()
+            return redirect('home')
+    else:
+        form=Form()
+    return render(request,'index.html',{'form':form,'data':data})
